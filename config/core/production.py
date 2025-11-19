bc8a52e77b7a8f9df862788acaf6f24d63b910c0
👨‍💻 Mathias Elorm <mathiaselorm@gmail.com>
2025-11-19 12:26:32 +0000

    production configurations

--- config/core/production.py
+++ config/core/production.py
@@ -4,17 +4,16 @@ from config.env import env
 
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ["*"]
 
-# # ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=["*"])
 
-# if not DEBUG and not ALLOWED_HOSTS:
-#     raise RuntimeError("ALLOWED_HOSTS must be set in production")
+if not DEBUG and not ALLOWED_HOSTS:
+    raise RuntimeError("ALLOWED_HOSTS must be set in production")
 
 DJANGO_REST_PASSWORDRESET_NO_INFORMATION_LEAKAGE = True
 
 
-# FRONTEND_URL = "http://localhost:3000"
+FRONTEND_URL = "http://localhost:3000"
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = env.list(
